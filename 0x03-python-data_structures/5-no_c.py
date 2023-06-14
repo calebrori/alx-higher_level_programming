5b49f1ffeece0ed0ff3de27377dedc0bb6ecc0dc
calebrori <calebrori@gmail.com>
2023-06-14 01:02:24 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 
 
 def no_c(my_string):
-"""This removes all characters c and C from a string"""
+    """This removes all characters c and C from a string"""
     duplicate = [x for x in my_string if x != 'c' and x != 'C']
     return ("".join(duplicate))
