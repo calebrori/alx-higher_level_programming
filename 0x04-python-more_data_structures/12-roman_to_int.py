df7709d5b45c17faad8bf9988214440a55504233
calebrori <calebrori@gmail.com>
2023-06-16 18:35:38 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,13 @@
 #!/usr/bin/python3
-def to_subtract(olnum):
-    to_sub = 0
-    a_list = max(olnum)
-
-    for n in olnum:
-        if a_list > n:
-            to_sub += n
-
-    return (a_list - to_sub)
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    r_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    ol_keys = list(r_num.keys())
-
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    finrom = 0
-    olnum = [0]
-
-    for c in roman_string:
-        for r_num in ol_keys:
-            if r_num == c:
-                if r_num.get(c) <= finrom:
-                    num += to_subtract(olnum)
-                    olnum = [r_num.get(c)]
-                else:
-                    olnum.append(r_num.get(c))
-
-                finrom = r_num.get(c)
-
-    num += to_subtract(olnum)
-
-    return (num)
+    last_rom = 0
+    for ch in roman_string:
+        curr_rom = rom_n.get(ch, 0)
+        if curr_rom > last_rom:
+            num += curr_rom - 2 * last_rom
+        else:
+            num += curr_rom
+        last_rom = curr_rom
+    return num
