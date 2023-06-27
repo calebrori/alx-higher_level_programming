f147d6ba18b334e7c79da9c429c5468cde77ede5
caleb.rori <calebrori@gmail.com>
2023-06-27 01:09:02 +0000

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    Divides two integers and prints the result.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        float: The result of the division, or None if an error occurs.
+
+    Raises:
+        ZeroDivisionError: If the denominator is zero.
+        TypeError: If either input is not an integer.
+
+    Prints:
+        The result of the division, or an error message if an error occurs.
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+        return None
+    except TypeError:
+        print("Both inputs must be integers")
+        return None
+    else:
+        return result
+    finally:
+        print("Inside result: {}".format(result))
