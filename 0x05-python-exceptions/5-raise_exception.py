f0e7cedce177a953ac7466e87f06d629bab5cfbd
caleb.rori <calebrori@gmail.com>
2023-06-27 02:03:53 +0000

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    """Raise a TypeError exception."""
+    """
+    This function raises a TypeError exception.
+
+    Raises:
+    TypeError: Always raised when the function is called.
+    """
+    # Raise a TypeError exception
     raise TypeError
+    except TypeError as e:
+        # Log the error
+        print(f"Error: {e}")
