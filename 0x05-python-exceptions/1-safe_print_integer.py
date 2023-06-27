7dc1d9f53645cb91270004e537b7b77558f08f65
caleb.rori <calebrori@gmail.com>
2023-06-27 00:08:10 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,7 +8,7 @@ def safe_print_integer(value):
     value (any): The value to be printed as an integer
 
     Returns:
-    bool: True if the value is an integer and has been correctly printed, False otherwise
+    bool: True if the value is an integer, False otherwise
     """
     try:
         # Try to convert the value to an integer and print it
