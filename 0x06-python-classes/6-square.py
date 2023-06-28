ff65fdb36d20f917ddd86a9cf19fc049668958fc
caleb.rori <calebrori@gmail.com>
2023-06-28 02:25:12 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,21 +27,19 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
         """Get the current size of the square."""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2
         or not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """Return the current area of the square."""
