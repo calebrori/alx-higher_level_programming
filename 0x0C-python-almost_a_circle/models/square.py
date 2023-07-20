75209083520a969bb6cdb15150e43cc6067b125c
calebrori <32507245+calebrori@users.noreply.github.com>
2023-07-20 20:35:04 +0300

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor initializes new Square
+
+        Args:
+            size (int): Square size
+            x (int): Square x axis
+            y (int): Square y axis
+            id (int): Square identity
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Puts the size of the Square"""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
