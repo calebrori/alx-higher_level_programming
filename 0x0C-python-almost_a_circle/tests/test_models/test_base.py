9d7099c0d3b4c5a289f97049a272f7973aed5cfe
calebrori <32507245+calebrori@users.noreply.github.com>
2023-07-22 01:22:29 +0300

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines unittests for base.py."""
-
 import os
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
