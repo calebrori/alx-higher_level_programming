ea0f1a23275cbc59f66fdb13092cf92861add50c
calebrori <calebrori@gmail.com>
2023-09-13 15:11:06 +0000

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let len = list.length - 1;
-    let c = 0;
-    while ((len - c) > 0) {
-      const aux = list[len];
-      list[len] = list[i];
-      list[i] = aux;
-      c++;
-      len--;
-    }
-    return list;
-  };
-  
+  let len = list.length - 1;
+  let c = 0;
+  while ((len - c) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    c++;
+    len--;
+  }
+  return list;
+};
