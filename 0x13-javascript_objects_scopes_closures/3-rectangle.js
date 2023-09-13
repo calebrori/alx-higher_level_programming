f30d6d587af7b9d9d1f6655534bf22d3b9610ebe
calebrori <calebrori@gmail.com>
2023-09-13 14:52:35 +0000

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let c = 0; c < this.height; c++) {
-        let d = '';
-        for (let e = 0; e < this.width; e++) {
-          d += 'X';
-        }
-        console.log(s);
-      }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  print () {
+    for (let c = 0; c < this.height; c++) {
+      let d = '';
+      for (let e = 0; e < this.width; e++) {
+        d += 'X';
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Rectangle;
