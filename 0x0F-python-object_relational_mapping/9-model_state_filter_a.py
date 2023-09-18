a91dc5eda8ed272f70cf00def9e191d64c336072
calebrori <32507245+calebrori@users.noreply.github.com>
2023-09-18 06:00:40 +0300

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all objects that contain the letter a from the database """
+""" script that lists all objects with letter a from the database """
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
