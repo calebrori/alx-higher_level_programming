50a634900b604e19991f7c0db75eca4e88b4c14f
calebrori <32507245+calebrori@users.noreply.github.com>
2023-09-28 16:50:22 +0300

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and displays all HTTP methods the server will accept
-curl "$1" -sX POST -d "email=calebrori@gmail.com&subject=This is a test email"
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
